11e7310dfbd8566254eb48d413432c3999e98f19
PivotalDan <dmearlsjr@pivotal.io>
2016-09-17 12:55:30 -0400

    Update opportunityq3.sql

--- opportunityq3.sql
+++ opportunityq3.sql
@@ -1,3 +1,5 @@
+SFDC Examples for SQL Code in Google Spreadsheets
+
 SELECT Account_Name__c,Amount,CloseDate,FiscalQuarter,FiscalYear,StageName,Name FROM Opportunity where  CloseDate> 2016-10-01 and CloseDate < 2017-01-01 AND Account_Name__c != 'The Manufacturers Life Insurance Company' AND Account_Name__c != 'EMC Corporation'  AND StageName  != 'Closed Lost'
 
 --Q4 Select Excel SFDC Enabler Code

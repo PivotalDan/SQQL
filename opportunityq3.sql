023392a213eafb74b8ce2c1f5267271e2919fd36
PivotalDan <dmearlsjr@pivotal.io>
2016-09-17 12:52:01 -0400

    Q3 Opportunity SOQL
    SFDC Q3 Opportunity SQL

---
+++ opportunityq3.sql
@@ -0,0 +1,1 @@
+SELECT Account_Name__c,Amount,CloseDate,FiscalQuarter,FiscalYear,StageName,Name FROM Opportunity where  CloseDate> 2016-10-01 and CloseDate < 2017-01-01 AND Account_Name__c != 'The Manufacturers Life Insurance Company' AND Account_Name__c != 'EMC Corporation'  AND StageName  != 'Closed Lost'

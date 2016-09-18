aa22c30f4085c900664d8f441a5d39b8ce3af3ee
PivotalDan <dmearlsjr@pivotal.io>
2016-09-18 19:32:53 -0400

    Added Forecast Stage
    Forecast Stage

--- opportunityq3.sql
+++ opportunityq3.sql
@@ -4,3 +4,9 @@ SELECT Account_Name__c,Amount,CloseDate,FiscalQuarter,FiscalYear,StageName,Name 
 
 --Q4 Select Excel SFDC Enabler Code
 SELECT Account_Name__c,Amount,CloseDate,FiscalQuarter,FiscalYear,StageName,Name FROM Opportunity where  CloseDate> 2016-10-01 and CloseDate < 2017-01-01 AND Account_Name__c != 'The Manufacturers Life Insurance Company' AND Account_Name__c != 'EMC Corporation'  AND StageName  != 'Closed Lost'
+
+--W Product
+SELECT Account_Name__c,Amount,CloseDate,FiscalQuarter,FiscalYear,StageName,Name,Product_Concat__c FROM Opportunity where  CloseDate> 2016-10-01 and CloseDate < 2017-01-01 AND Account_Name__c != 'The Manufacturers Life Insurance Company' AND Account_Name__c != 'EMC Corporation'  AND StageName  != 'Closed Lost'
+
+--Added Forecast Status
+SELECT Account_Name__c,Amount,CloseDate,FiscalQuarter,FiscalYear,StageName,Forecast_Status__c,Name,Product_Concat__c FROM Opportunity where  CloseDate> 2016-10-01 and CloseDate < 2017-01-01 AND Account_Name__c != 'The Manufacturers Life Insurance Company' AND Account_Name__c != 'EMC Corporation'  AND StageName  != 'Closed Lost'
